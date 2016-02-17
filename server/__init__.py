0ff94801784f6a8c0b75b509cad17e31262e6eb8
Mathieu David <mathieudavid@mathieudavid.org>
2016-02-17 20:12:59 +0100

    manage exception when port is already in use

--- server/__init__.py
+++ server/__init__.py
@@ -1,29 +1,31 @@
-#Available ==> message ok et username
-#liste vide ==> remplir adresse ip et username
-#si il est dans la liste erreur
-#sinon message ok
-
-import pickle
 import socket
-import struct
 import sys
 
 addressserveur = (socket.gethostname(), 6000)
+
 
 class Server:
 
     def __init__(self):
         self.socket = socket.socket()
-        self.socket.bind(addressserveur)
+
+        # Try to bind to a specific port, handle if port is already in use
+        try:
+            self.socket.bind(addressserveur)
+        except socket.error as e:
+            if e.errno == 98:
+                print("Port is already in use")
+                sys.exit(1)
+
         self.address = socket.gethostname()
         self.user = {}
 
     def run(self):
         print("Listening on " + addressserveur[0] + ":%i" % addressserveur[1])
-        #Ecoute sur un port
+
         self.socket.listen(0)
+
         while True:
-            #Renvoi socket sous forme de tuple client, ip
             client, addressip = self.socket.accept()
             try:
                 self._handle(client)
@@ -35,7 +37,6 @@ class Server:
     def _handle(self, client):
         response = self._recv(client)
         print(response)
-
 
     def _recv(self, client):
         response = b""
